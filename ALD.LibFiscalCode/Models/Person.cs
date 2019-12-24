2d51a2e551b66c612e87b57ed2f066bb29e66812
Andrea Luciano Damico <aldamico90@gmail.com>
2019-12-24 02:03:00 +0100

    Initial version of Person model

---
+++ ALD.LibFiscalCode/Models/Person.cs
@@ -0,0 +1,22 @@
+﻿using ALD.LibFiscalCode.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ALD.LibFiscalCode.Models
+{
+    public class Person
+    {
+        public Person()
+        {
+            Name = "";
+            Surname = "";
+            DateOfBirth = DateTime.Now;
+            Gender = Gender.Unspecified;
+        }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public Gender Gender { get; set; }
+    }
+}
