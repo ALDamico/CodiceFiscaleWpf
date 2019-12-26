c5a14517ae0213b86c8ebb79ddbec1a5622b3cb8
Andrea Luciano Damico <aldamico90@gmail.com>
2019-12-27 00:19:35 +0100

    Initial version of PlacesList

---
+++ ALD.LibFiscalCode/ViewModels/PlacesListViewModel.cs
@@ -0,0 +1,59 @@
+﻿using ALD.LibFiscalCode.Persistence.Models;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+
+namespace ALD.LibFiscalCode.ViewModels
+{
+    public class PlacesListViewModel : AbstractNotifyPropertyChanged
+    {
+        public PlacesListViewModel()
+        {
+            SelectedPlace = null;
+        }
+
+        public PlacesListViewModel(ICollection<Place> places)
+        {
+            SelectedPlace = null;
+            Places = new ObservableCollection<Place>(places);
+            FilteredPlaces = new ObservableCollection<Place>(places);
+        }
+
+        public ObservableCollection<Place> Places { get;  }
+
+        public Place SelectedPlace
+        {
+            get => selectedPlace; 
+            set
+            {
+                selectedPlace = value;
+                OnPropertyChanged(nameof(SelectedPlace));
+            }
+        }
+
+        public void FilterPlaces(string filter)
+        {
+            FilteredPlaces = new ObservableCollection<Place>(Places.Where(p => p.Name.Contains(filter, System.StringComparison.InvariantCultureIgnoreCase)));
+        }
+
+        public void ResetFilter()
+        {
+            FilteredPlaces = new ObservableCollection<Place>(Places);
+        }
+
+        public ObservableCollection<Place> FilteredPlaces { 
+        get
+            {
+                return filteredPlaces;
+            }
+            set
+            {
+                filteredPlaces = value;
+                OnPropertyChanged(nameof(FilteredPlaces));
+            }
+        }
+
+        private ObservableCollection<Place> filteredPlaces;
+        private Place selectedPlace;
+    }
+}
---
+++ CodiceFiscaleUI/PlacesListView/PlacesList.xaml.cs
@@ -0,0 +1,72 @@
+﻿using ALD.LibFiscalCode.Persistence.Models;
+using ALD.LibFiscalCode.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CodiceFiscaleUI.PlacesListView
+{
+    /// <summary>
+    /// Interaction logic for PlacesList.xaml
+    /// </summary>
+    public partial class PlacesList : Window
+    {
+        public PlacesList(ICollection<Place> places)
+        {
+            viewModel = new PlacesListViewModel(places);
+            DataContext = viewModel;
+            loading = true;
+            InitializeComponent();
+        }
+
+        private bool loading;
+
+        private PlacesListViewModel viewModel;
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (e == null)
+            {
+                return;
+            }
+
+            var selectedPlace = e.AddedItems[0] as Place;
+            viewModel.SelectedPlace = selectedPlace;
+        }
+
+        
+       
+
+        private void txtFilter_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (loading)
+            {
+                loading = false;
+                return;
+            }
+            if (string.IsNullOrEmpty(txtFilter.Text))
+            {
+                viewModel.ResetFilter();
+            }
+            else {
+                viewModel.FilterPlaces(txtFilter.Text);
+                
+            }
+            
+        }
+
+        private void txtFilter_GotFocus(object sender, RoutedEventArgs e)
+        {
+            txtFilter.Text = "";
+            txtFilter.Foreground = Brushes.Black;
+        }
+    }
+}
