8232786dfb7c70bf75d3c5924554fcddbc7b8d21
Andrea Luciano Damico <aldamico90@gmail.com>
2020-01-07 02:54:43 +0100

    Restore feature complete

---
+++ ALD.LibFiscalCode/Progress/ResetProgress.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace ALD.LibFiscalCode.Progress
+{
+    public class ResetProgress
+    {
+        public int CompletedTasks { get; set; } = 1;
+        public List<string> TaskDescriptions { get; } = new List<string>();
+        public string CurrentTask { get; set; }
+
+        public int Percentage => CompletedTasks * 100 / TaskDescriptions.Count;
+    }
+}
