18c3701bc94aed73eaac28e36dfc3ac04bcf696c
Andrea Luciano Damico <aldamico90@gmail.com>
2020-01-10 00:28:12 +0100

    Added settings to cleanup script Feature: can now move data source to its intended destination

--- PlacesImporter/cleanup.py
+++ PlacesImporter/cleanup.py
@@ -3,7 +3,8 @@
 import sqlite3
 import csv
 import logging
-import datetime
+from datetime import datetime
+from shutil import copy
 
 
 class Place(object):
@@ -24,10 +25,36 @@ class Place(object):
         return (self.name, self.province, self.province_abbreviation, self.region, self.code)
 
 
-start_time = datetime.datetime.now()
+class LocalizedString(object):
+    id: int
+    name: str
+    value: str
+    language_id: str
+    window_id: str
+
+    def __init__(self, *args, **kwargs):
+        if len(kwargs) > 0:
+            self.id = kwargs["id"]
+            self.name = kwargs["name"]
+            self.value = kwargs["value"]
+            self.language_id = kwargs["language_id"]
+            self.window_id = kwargs["window_id"]
+        elif len(args) > 0:
+            self.id = args[0]
+            self.name = args[1]
+            self.value = args[2]
+            self.language_id = args[3]
+            self.window_id = args[4]
+
+    def get_as_tuple(self):
+        return (self.name, self.value, self.language_id, self.window_id)
+
+
+start_time = datetime.now()
 
 log_file = "cleanup.log"
-csv_file_name = "data_source_dump.csv"
+csv_file_name = "places.csv"
+localized_strings_file_name = "localizedStrings.csv"
 logging.basicConfig(filename=log_file, level=logging.INFO, filemode='w')
 logging.log(logging.INFO, "Started cleanup at {}".format(start_time))
 
@@ -41,7 +68,9 @@ drop_queries = (
     "DROP TABLE People",
     "DROP TABLE Places",
     "DROP TABLE Languages",
-    "DROP TABLE LocalizedStrings"
+    "DROP TABLE LocalizedStrings",
+    "DROP TABLE Windows",
+    "DROP TABLE Settings"
 )
 
 
@@ -114,6 +143,15 @@ ddl_queries = (
             FOREIGN KEY (language_id) REFERENCES Languages(id) ON DELETE SET NULL,
             FOREIGN KEY (window_id) REFERENCES Windows(id) ON DELETE SET NULL
         )
+    """,
+    """
+        CREATE TABLE Settings
+        (
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            name TEXT,
+            string_value TEXT,
+            int_value INTEGER
+        )
     """
 )
 
@@ -144,7 +182,29 @@ for el in languages_default_values:
     logging.log(logging.INFO, query)
     conn.execute(query, (el["name"], el["iso_2_code"], el["iso_3_code"]))
 
-with open(csv_file_name, encoding='utf-8') as csv_file:
+window_queries = [
+    "INSERT INTO Windows (id,window_name) VALUES (1,'MainWindow')",
+    "INSERT INTO Windows (id,window_name) VALUES (2,'AboutWindow')",
+    "INSERT INTO Windows (id,window_name) VALUES (3,'DatePickerWindow')",
+    "INSERT INTO Windows (id,window_name) VALUES (4,'PlacesImportView')",
+    "INSERT INTO Windows (id,window_name) VALUES (5,'PlacesList')",
+    "INSERT INTO Windows (id,window_name) VALUES (6,'SettingsWindow')"
+]
+
+for query in window_queries:
+    conn.execute(query)
+
+settings_queries = [
+    "INSERT INTO Settings (name,int_value,string_value) VALUES ('AppLanguage',1,NULL)",
+    "INSERT INTO Settings (name,int_value,string_value) VALUES ('DataSourceLocation',NULL,NULL)",
+    "INSERT INTO Settings (name,int_value,string_value) VALUES ('MaxHistorySize',0,NULL)",
+    "INSERT INTO Settings (name,int_value,string_value) VALUES ('DefaultDate',NULL,NULL)"
+]
+
+for query in settings_queries:
+    conn.execute(query)
+
+with open(localized_strings_file_name, encoding='utf-8') as csv_file:
     dialect = csv.Sniffer().sniff(csv_file.read(1024))
     place_reader = csv.reader(csv_file, dialect)
 
@@ -152,12 +212,31 @@ with open(csv_file_name, encoding='utf-8') as csv_file:
     next(place_reader, None)
     for row in place_reader:
         logging.log(logging.INFO, "Inserting place {}".format(row[0]))
+        query = """
+            INSERT INTO LocalizedStrings(name, value, language_id, window_id)
+            VALUES(?, ?, ?, ?)
+        """
+        values = tuple(row[1:5])
+        conn.execute(query, values)
+
+    conn.commit()
+
+with open(csv_file_name, encoding='utf-8') as csv_file:
+    dialect = csv.Sniffer().sniff(csv_file.read(1024))
+    strings_reader = csv.reader(csv_file, dialect)
+
+    csv_file.seek(0)
+    next(strings_reader, None)
+    for row in strings_reader:
+        logging.log(
+            logging.INFO, "Inserting localized string {}".format(row[0]))
         name = row[0]
         province = row[1]
         province_abbreviation = row[2]
         region_name = row[3]
         code = row[4]
-        place = Place(name, province, province_abbreviation, region_name, code)
+        place = Place(
+            name, province, province_abbreviation, region_name, code)
         query = """
             INSERT INTO Places(name, province_name, province_abbreviation, region_name, code)
             VALUES (?, ?, ?, ?, ?)
@@ -180,6 +259,12 @@ for query in index_queries:
 
 conn.commit()
 conn.close()
-logging.log(logging.INFO, "Completed at {}".format(datetime.datetime.now()))
+prompt = input("Move data source to its destination? Y/N")
+if prompt.lower() == 'y':
+    logging.info(
+        "Copying {} to ../ALD.LibFiscalCode.Persistence/DataSource/".format(database_file))
+    copy(database_file, "../ALD.LibFiscalCode.Persistence/DataSource/")
+
+logging.log(logging.INFO, "Completed at {}".format(datetime.now()))
 logging.shutdown()
 exit(0)
