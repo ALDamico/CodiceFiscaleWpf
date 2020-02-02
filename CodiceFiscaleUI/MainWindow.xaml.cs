7f0d8c6bb87824197aca3a8bbf14c20af1d9f4dd
Andrea Luciano Damico <aldamico90@gmail.com>
2020-02-02 17:30:00 +0100

    About window unaccessible because not yet implemented properly, added website url

--- CodiceFiscaleUI/MainWindow.xaml.cs
+++ CodiceFiscaleUI/MainWindow.xaml.cs
@@ -12,9 +12,10 @@ using CodiceFiscaleUI.AboutView;
 using CodiceFiscaleUI.DatePicker;
 using CodiceFiscaleUI.PlaceImport;
 using CodiceFiscaleUI.PlacesListView;
-using CodiceFiscaleUI.ArchiveWindow;
+using ArchiveWindow = CodiceFiscaleUI.ArchiveWindow.ArchiveWindow;
 using Microsoft.Win32;
 using Localization = ALD.LibFiscalCode.Localization.Localization;
+using System.Diagnostics;
 
 namespace CodiceFiscaleUI
 {
@@ -256,6 +257,21 @@ namespace CodiceFiscaleUI
         {
             var archiveWin = new ArchiveWindow.ArchiveWindow();
             archiveWin.Show();
+        }
+
+        private void MnuShowOmocodes_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MnuViewOmocodes_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void MnuOpenWebsite_Click(object sender, RoutedEventArgs e)
+        {
+            //Broken in dotnet core, has to use this trick from here: https://brockallen.com/2016/09/24/process-start-for-urls-on-net-core/
+            var url = "https://www.lucianodamico.info";
+            Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
         }
     }
 }
