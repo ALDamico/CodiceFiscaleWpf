d9001f1d1e77cd8e34ac34da1136da3eff61c249
Andrea Luciano Damico <aldamico90@gmail.com>
2020-02-22 13:41:33 +0100

    Started working on Fiscal Code validation Modified IValidator interface

--- ALD.LibFiscalCode/Validators/FiscalCodeValidator.cs
+++ ALD.LibFiscalCode/Validators/FiscalCodeValidator.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections.Generic;
+using ALD.LibFiscalCode.Builders;
+using ALD.LibFiscalCode.Persistence.Models;
 
 namespace ALD.LibFiscalCode.Validators
 {
     public class FiscalCodeValidator:IValidator
     {
-        public FiscalCodeValidator()
+        public FiscalCodeValidator(FiscalCode fiscalCode, Person person)
         {
             ValidationMessages = new List<ValidationResult>();
-            
+            builder = new FiscalCodeBuilder(person);
         }
         public bool IsValid { get; }
         public List<ValidationResult> ValidationMessages { get; }
@@ -20,7 +22,8 @@ namespace ALD.LibFiscalCode.Validators
         {
             throw new System.NotImplementedException();
         }
-        
+
+        private FiscalCodeBuilder builder;
         
     }
 }
